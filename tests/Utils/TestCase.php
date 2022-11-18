554bdcd5f6d0d160b6c188c3f2baefc07e0ffefa
Miguel Morillo <miguelmwilliams@gmail.com>
2022-11-18 19:55:33 +0100

    improvement: remove old code

--- tests/Utils/TestCase.php
+++ tests/Utils/TestCase.php
@@ -1,23 +1,9 @@
 <?php
 
-namespace Microservices\Tests\Utils;
+namespace PhpLibraryTemplate\Tests\Utils;
 
-use Exception;
 use PHPUnit\Framework\TestCase as UnitTestCase;
-use Selective\TestTrait\Traits\ContainerTestTrait;
-use Selective\TestTrait\Traits\MockTestTrait;
 
 class TestCase extends UnitTestCase
 {
-    use ContainerTestTrait;
-    use MockTestTrait;
-
-    /**
-     * @throws Exception
-     */
-    protected function setUp(): void
-    {
-        $container = ContainerFactory::buildContainer(__DIR__ . '/../../config/settings.php');
-        $this->setUpContainer($container);
-    }
 }
